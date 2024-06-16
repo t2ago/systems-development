57b2a0538d506a3e8b432f82e5f575b208da85c7
yo <161033000+t2ago@users.noreply.github.com>
2024-06-16 04:51:26 -0300

    Delete python/exercises directory

--- python/exercises/exercises 36.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system("cls||clear")
-
-for i in range(100,121):
-    if i % 2 == 0:
-        print(f"Números pares: {i}")
--- python/exercises/exercises 42.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system("cls||clear")
-
-idade = int(input("Digite sua idade: "))
-
-if idade >= 18 and idade < 65 :
-    print("Você é obrigado a votar!")
-else :
-    print("Você NÃO é obrigado a votar!")
