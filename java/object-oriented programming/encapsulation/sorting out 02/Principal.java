4c82ae59a9a1f268a9c8942c4e9dff8a7c77a87d
tiago <161033000+t2ago@users.noreply.github.com>
2024-08-07 10:50:46 -0300

    Add files via upload

---
+++ java/object-oriented programming/encapsulation/sorting out 02/Principal.java
@@ -0,0 +1,16 @@
+package com.mycompany.separando2;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Funcionarios funcionario = new Funcionarios();
+        
+        funcionario.setNome("José");
+        funcionario.setIdade(20);
+        funcionario.setSalario(3000);
+        
+        System.out.println("Nome: " + funcionario.getNome());
+        System.out.println("Idade: " + funcionario.getIdade());
+        System.out.println("Sálario: " + funcionario.getSalario());
+    }
+}
