7f97df76f948595633cef7a8847d460c4b92e8b0
tiago <161033000+t2ago@users.noreply.github.com>
2024-08-07 09:51:50 -0300

    Add files via upload

---
+++ java/exercise 06.java
@@ -0,0 +1,14 @@
+package com.mycompany.comparacaotipos;
+
+public class ComparacaoTipos {
+    public static void main(String[] args) {
+        String nomeUsuario = "Marta";
+        int senha = 123;
+        
+        boolean resultadoNome = nomeUsuario.equals("Marta");
+        boolean resultadoSenha = (senha == 456);
+        
+        System.out.println("O nome do usuário está correto?  " + resultadoNome);
+        System.out.println("A senha está correta?  " + resultadoSenha);
+    }
+}
---
+++ java/exercise 08.java
@@ -0,0 +1,15 @@
+package com.mycompany.operadoreslogicos;
+
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+        boolean temSol = true;
+        boolean folga = false;
+        
+        if (temSol && folga) {
+            System.out.println("Irei passear hoje! =D");
+        } else {
+            System.out.println("Não irei passear! =C");
+        }
+    }
+}
