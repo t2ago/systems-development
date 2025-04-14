371de9fe894f03d0f9315d3c3ac911b3ca5dcf63
t2ago <161033000+t2ago@users.noreply.github.com>
2025-04-14 11:32:41 -0300

    Add files via upload

---
+++ java/object-oriented programming/interface/interface07/src/main/java/com/mycompany/exemplo_02/exemplo_02.java
@@ -0,0 +1,17 @@
+package com.mycompany.exemplo_02;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno.den
+ */
+public class exemplo_02 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
