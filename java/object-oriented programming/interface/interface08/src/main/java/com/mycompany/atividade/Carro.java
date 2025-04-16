2322010f2c04bc1aeb1f1641f8f5c8436fe6f249
t2ago <161033000+t2ago@users.noreply.github.com>
2025-04-16 10:01:58 -0300

    Add files via upload

---
+++ java/object-oriented programming/interface/interface08/src/main/java/com/mycompany/atividade/Carro.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atividade;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Carro extends Veiculo {
+    private int portas;
+
+    public Carro(int portas, String marca, String modelo) {
+        super(marca, modelo);
+        this.portas = portas;
+    }
+
+    public int getPortas() {
+        return portas;
+    }
+
+    public void setPortas(int portas) {
+        this.portas = portas;
+    }
+}
