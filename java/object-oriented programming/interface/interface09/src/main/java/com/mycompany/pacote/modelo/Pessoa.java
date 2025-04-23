02f79b2a80cc556c37212e55b15c1979321d71a1
t2ago <161033000+t2ago@users.noreply.github.com>
2025-04-23 11:28:14 -0300

    Add files via upload

---
+++ java/object-oriented programming/interface/interface09/src/main/java/com/mycompany/pacote/modelo/Pessoa.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pacote.modelo;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
